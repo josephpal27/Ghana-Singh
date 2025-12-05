af3d2d0e196460798ed4436d9fed82a59ea189c0
Joseph Pal <josephpal27@gmail.com>
2025-12-05 16:21:10 +0530

    Best Sellers added

--- src/components/best-sellers/BestSellers.jsx
+++ src/components/best-sellers/BestSellers.jsx
@@ -1,11 +1,56 @@
 import './BestSellers.css';
+import sellerImg1 from '../../assets/images/sellers/1.png';
+import sellerImg2 from '../../assets/images/sellers/2.png';
+import sellerImg3 from '../../assets/images/sellers/3.png';
 
 const BestSellers = () => {
-  return (
-    <>
-      
-    </>
-  )
+
+    const sellersData = [
+        {
+            id: 1,
+            img: sellerImg1,
+            title: "Lorem Ipsum",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt eiusmod ut labore et dolore magna eiusmod aliqua.",
+        },
+        {
+            id: 2,
+            img: sellerImg2,
+            title: "Lorem Ipsum",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod eiusmod tempor incididunt ut labore et dolore magna eiusmod aliqua.",
+        },
+        {
+            id: 3,
+            img: sellerImg3,
+            title: "Lorem Ipsum",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt eiusmod ut labore et dolore magna eiusmod aliqua.",
+        },
+    ]
+
+    return (
+        <>
+            <div className="best-sellers">
+                <h4>BEST SELLERS</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="sellers-card-row">
+                    {
+                        sellersData.map((item, index) => {
+                            return (
+                                <div className="seller-card" key={index}>
+                                    <div className="card-image">
+                                        <img src={item.img} alt={`Seller ${item.id}`} loading="lazy" />
+                                    </div>
+                                    <div className="card-desc">
+                                        <span>{item.title}</span>
+                                        <p>{item.desc}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default BestSellers
