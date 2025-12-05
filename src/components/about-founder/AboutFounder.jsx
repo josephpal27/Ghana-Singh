b831e5d30a301ac8472d5a37f09dbdc6219bd7bc
Joseph Pal <josephpal27@gmail.com>
2025-12-05 15:53:47 +0530

    Featured Collections added

--- src/components/about-founder/AboutFounder.jsx
+++ src/components/about-founder/AboutFounder.jsx
@@ -11,13 +11,17 @@ const AboutFounder = () => {
                 </div>
                 <div className="about-founder-content">
                     <div className="content-box">
-                        <p><span className="left-quote">“</span> He strongly believes in the idea that “once you come to GhanaSingh, <span className="right-quote">”</span></p>
+                        <span className="left-quote">“</span>
+                        <p>He strongly believes in the idea that “once you come to GhanaSingh,</p>
+                        <span className="right-quote">”</span>
                     </div>
                     <div className="content-box">
                         <img src={founderImg} alt="Founder" loading="lazy" />
                     </div>
                     <div className="content-box">
-                        <p><span className="left-quote">“</span> you won't go anywhere else,” reflecting his confidence, loyalty, and deep dedication to what he stands for. <span className="right-quote">”</span></p>
+                        <span className="left-quote">“</span>
+                        <p>you won't go anywhere else,” reflecting his confidence, loyalty, and deep dedication to what he stands for.</p>
+                        <span className="right-quote">”</span>
                     </div>
                 </div>
             </section>
--- src/components/collections/Collections.jsx
+++ src/components/collections/Collections.jsx
@@ -1,9 +1,48 @@
 import './Collections.css';
+import collectionImg1 from '../../assets/images/collections/1.avif';
+import collectionImg2 from '../../assets/images/collections/2.avif';
+import collectionImg3 from '../../assets/images/collections/3.avif';
+import collectionImg4 from '../../assets/images/collections/4.avif';
 
 const Collections = () => {
   return (
     <>
-      
+      <section className="collections">
+        <div className="collections-head">
+            <h3>FEATURED COLLECTIONS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore eiusmod et dolore magna aliqua. Quis ipsum incididunt suspendisse ultrices consectetur gravida.</p>
+        </div>
+        <div className="collection-grid">
+            {/* Left */}
+            <div className="collection-column">
+                <div className="box text-box">
+                    <span className="left-quote">“</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur.</p>
+                    <span className="right-quote">”</span>
+                </div>
+                <div className="box">
+                    <img src={collectionImg1} alt="Collection 1" loading="lazy" />
+                </div>
+                <div className="box">
+                    <img src={collectionImg2} alt="Collection 2" loading="lazy" />
+                </div>
+            </div>
+            {/* Right */}
+            <div className="collection-column">
+                <div className="box">
+                    <img src={collectionImg3} alt="Collection 3" loading="lazy" />
+                </div>
+                <div className="box">
+                    <img src={collectionImg4} alt="Collection 4" loading="lazy" />
+                </div>
+                <div className="box text-box">
+                    <span className="left-quote">“</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur.</p>
+                    <span className="right-quote">”</span>
+                </div>
+            </div>
+        </div>
+      </section>
     </>
   )
 }
