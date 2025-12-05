6e3c41eca1c1ddb6db10aef18666823aca3968a8
Joseph Pal <josephpal27@gmail.com>
2025-12-05 17:13:47 +0530

    Footer Updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,25 @@
 import './Footer.css';
+import logo from '../../assets/images/logo/logo.png';
 
 const Footer = () => {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <footer>
+                <div className="footer-content">
+                    <div className="footer-left">
+                        <img src={logo} alt="Logo" loading="lazy" />
+                    </div>
+                    <div className="footer-right">
+                        <p>VISIT OUR STORE</p>
+                        <p>Shop no 1/6 shyam vihar, <br /> Gr floor, 706 linking road opposite Punjab Sindh paneer, Khar west mumbai 400052</p>
+                    </div>
+                </div>
+                <div className="copyright">
+                    <p>Copyright © 2025 | Ghana Singh | All Rights Reserved</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default Footer
