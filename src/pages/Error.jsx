bf3c3c4ea9e8e517d9408a1247a5446df530fb45
Joseph Pal <josephpal27@gmail.com>
2025-12-08 13:44:07 +0530

    Error Page Mobile View added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ import '../css/Error.css';
 const Error = () => {
     return (
         <>
-            <section className="error">
+            <section className="error" data-aos="fade">
                 <h1>PAGE NOT FOUND</h1>
                 <Link to="/"><button>GO TO HOME</button></Link>
             </section>
