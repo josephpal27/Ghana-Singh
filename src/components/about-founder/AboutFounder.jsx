755bbd58a12f94b7127306d778e6781f2b6769d1
Joseph Pal <josephpal27@gmail.com>
2026-05-25 15:36:21 +0530

    Founder Section Updated

--- src/components/about-founder/AboutFounder.jsx
+++ src/components/about-founder/AboutFounder.jsx
@@ -12,13 +12,35 @@ const AboutFounder = () => {
                     <span id="founder-background" data-aos="zoom-in" data-aos-once="true">FOUNDER</span>
                     <div className="content-box" data-aos="fade">
                         <span className="left-quote">“</span>
-                        <p>He strongly believes in the idea that “once you come to Ghanasingh,</p>
+                        <p>Carrying forward a legacy built over generations, Gautam Ghanasingh closely followed the artistry, technique and craft of jewellery making as a child. Holding a pen very early on, he started sketching pieces and later built on the finer nuances of jewellery design and making. A hobby that developed as a child, is today his deepest passion.</p>
+                        <p>With a keen eye for quality and aesthetics, besides being highly qualified and trained in the jewellery field, he adds his knowledge and expertise to create newer trends in jewellery with each of his designs. His vision is to create ‘one of a kind, unique pieces’ with great attention to detail that echo a harmonious blend between modern design and traditional craftsmanship.</p>
                     </div>
-                    <div className="content-box">
-                        <img src="/images/founder.avif" alt="Founder" loading="lazy" />
+                    <div className="content-box image-box">
+                        <img src="/images/founder-1.jpeg" alt="Founder 1" loading="lazy" />
+                        <p>Gautam Ghanasingh</p>
+                        <p>Creative Director, Ghanasingh Be True, Bandra</p>
                     </div>
                     <div className="content-box" data-aos="fade">
-                        <p>you won't go anywhere else,” reflecting his confidence, loyalty, and deep dedication to what he stands for.</p>
+                        <p>Crafting masterpieces for the woman of today, he creatively and strategically introduced contemporary styles in his traditional pieces to lend a modern touch. With an ethos underlined by belief in product innovation and creativity, Gautam believes that jewellery should be treasured as heirlooms and also as a versatile creation that is worthy of being adorned every day.</p>
+                        <p>As the creator of the brand, his adage remains to BE TRUE to himself.</p>
+                        <span className="right-quote">”</span>
+                    </div>
+                </div>
+                <div className="about-founder-content">
+                    <span id="founder-background" data-aos="zoom-in" data-aos-once="true">FOUNDER</span>
+                    <div className="content-box" data-aos="fade">
+                        <span className="left-quote">“</span>
+                        <p>At the head of any business lies a mind that is astute and for Ghanasingh Be True, it is their Expansion Strategy & Marketing Director Krishaa Ghanasingh.</p>
+                        <p>An extremely sound work portfolio as the financial advisor at JP Morgan, followed by Goldman Sachs and a decade of being an investment banker, Krishaa applied her commerce and finance prowess to jewellery and the transition was beyond smooth. As an avid art collector, she is well-informed about international trends, hence there is a decent understanding of design too. Being married to the heart of the brand Gautam Ghanasingh, Krishaa joined the family business to accomplish a shared vision to make Ghaasingh Be True a global brand rooted in India.</p>
+                    </div>
+                    <div className="content-box image-box">
+                        <img src="/images/founder-2.jpeg" alt="Founder 2" loading="lazy" />
+                        <p>Krishaa Ghanasingh</p>
+                        <p>Expansion Strategy & Marketing Director, Ghanasingh Be True</p>
+                    </div>
+                    <div className="content-box" data-aos="fade">
+                        <p>Married into a family of jewellers with a bent for business, the merger of the two holds unmatched value for the brand. From helming expansion strategy to being instrumental in formulating HR, administration and other processes, she is the backbone of the brand. Her precision in planning, foresightedness, innovative strategies and executional abilities are reflected in the brand’s overall progress. Lately, she assumed a vital role in catering to the discerning clientele from the nawaabi city of Hyderabad, the brand’s most recent retail footprint. Krishaa is now driving new initiatives and exploring growth opportunities for retail. This has immensely helped the fine jewellery label to foray into the international markets via e-commerce. Under her leadership, Ghanasingh Be True has evolved from strength to strength.</p>
+                        <p>Krishaa stresses on the ability of women to find their own space through hard work and perseverance. She believes that women, in general, will play a very crucial role in shaping the business environment in India over the next decade.</p>
                         <span className="right-quote">”</span>
                     </div>
                 </div>
